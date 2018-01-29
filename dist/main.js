4bbb056f46d8169467fd58a83d3927a9ead57b3e
gabrielamf <glennygabb@gmail.com>
2018-01-29 02:02:15 -0500

    añade package.json y babel

---
+++ dist/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+(function ($) {
+  $.fn.extend({
+    cardify: function cardify() {
+      var _this = this;
+
+      return this.each(function () {
+        $(_this).find('img').each(function (index, element) {
+          $(element).wrap('<figure></figure>');
+          var width = $(element).width();
+          var height = $(element).height(); // Coloca texto de alt en figcaption
+          var text = $(element).attr('alt');
+          $(element).css({
+            width: width
+          });
+          $(element).after('<figcaption>' + text + '</figcaption>');
+          $(element).next().addClass('figcaption').css({
+            width: width,
+            height: height,
+            'line-height': height + 'px'
+          }).hide();
+          $(element).parent().addClass('figure').css({
+            width: width,
+            height: height
+          }).mouseover(function () {
+            $(element).hide();
+            $(element).next().show();
+          }).mouseout(function () {
+            $(element).show();
+            $(element).next().hide();
+          });
+        });
+      });
+    }
+  });
+})(jQuery);
