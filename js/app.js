10db72cea6ec2000db4e5349a7b54faccab4fa96
gabrielamf <glennygabb@gmail.com>
2018-01-29 01:08:22 -0500

    añade funcion pluggin img

---
+++ js/app.js
@@ -0,0 +1,34 @@
+(function($) { 
+  $.fn.extend({
+    cardify: function() {
+      return this.each(() => {
+        $(this).find('img').each((index, element) => {
+          $(element).wrap('<figure></figure>');
+          let width = $(element).width();
+          let height = $(element).height(); // Coloca texto de alt en figcaption
+          let text = $(element).attr('alt');
+          $(element).css({
+            width: width,
+          });
+          $(element).after(`<figcaption>${text}</figcaption>`);
+          $(element).next().addClass('figcaption').css({
+            width: width,
+            height: height,
+            'line-height': (`${height}px`)
+          }).hide();
+          $(element).parent().addClass('figure').css({
+            width: width,
+            height: height
+          }).mouseover(() => {
+            $(element).hide();
+            $(element).next().show();
+          }).mouseout(() => {
+            $(element).show();
+            $(element).next().hide();
+          });
+        });
+      });
+    }
+  });
+})(jQuery);
+
